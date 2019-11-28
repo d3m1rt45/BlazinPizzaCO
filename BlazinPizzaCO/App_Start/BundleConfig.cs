633334b5fcdd95e0b0e2aeb11ad86c330bae142e
d3m1rt45 <d3m1rt45@outlook.com>
2019-11-28 12:08:04 +0000

    [unnecessary-files-deleted]

--- BlazinPizzaCO/App_Start/BundleConfig.cs
+++ BlazinPizzaCO/App_Start/BundleConfig.cs
@@ -9,22 +9,22 @@ namespace BlazinPizzaCO
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.3.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-2.8.3.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/styles.css",
                       "~/Content/fontawesome/all.css"));
         }
     }
